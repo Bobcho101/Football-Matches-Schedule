3081eba6ffa6271427912ed1c732b4103d0d695a
Bobcho101 <bgbobi878@gmail.com>
2025-04-13 23:23:28 +0300

    added a get handler for premier league

--- server/server.js
+++ server/server.js
@@ -16,12 +16,26 @@ app.get('/matches/cl/:apiKey', async (req, res) => {
                 'X-Auth-Token': apiKey, 
             },
         });
-
         res.json(response.data);
     } catch (err) {
         console.log(err.message);
     }
 });
+
+app.get('/matches/pl/:apiKey', async (req, res) => {
+    const apiKey = req.params.apiKey;
+    try {
+        const response = await axios.get('https://api.football-data.org/v4/competitions/PL/matches?status=SCHEDULED', {
+            headers: {
+                'X-Auth-Token': apiKey, 
+            },
+        });
+        res.json(response.data);
+    } catch (err) {
+        console.log(err.message);
+    }
+});
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
