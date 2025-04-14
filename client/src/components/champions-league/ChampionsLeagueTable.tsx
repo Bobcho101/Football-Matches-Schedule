6877bc060747b48902806e6a6bf60cab87a39987
Bobcho101 <bgbobi878@gmail.com>
2025-04-14 16:48:29 +0300

    added a bg image to the champitons league

--- client/src/components/champions-league/ChampionsLeagueTable.tsx
+++ client/src/components/champions-league/ChampionsLeagueTable.tsx
@@ -7,8 +7,8 @@ const ChampionsLeagueTable: React.FC = () => {
     console.log(matches);
     
     return (
-        <div className="p-6 bg-gray-100 min-h-screen">
-            <h2 className="text-3xl font-bold mb-6 text-gray-800 text-center">Upcoming Matches</h2>
+        <div className="p-6 bg-[url('/CL-bg.jpg')] bg-cover bg-center min-h-screen">
+            <h2 className="text-3xl font-bold mb-6 text-white text-center">Upcoming Matches</h2>
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
                 <div className="bg-white p-4 rounded-xl shadow-md hover:shadow-lg transition duration-300">
                 <div className="text-center text-xl font-semibold text-gray-700 mb-2">
