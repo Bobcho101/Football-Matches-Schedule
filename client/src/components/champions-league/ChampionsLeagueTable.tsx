05576d92a040b4a3c4829ab04b4b486511903c7b
Bobcho101 <bgbobi878@gmail.com>
2025-04-15 18:05:00 +0300

    added an error handling message

--- client/src/components/champions-league/ChampionsLeagueTable.tsx
+++ client/src/components/champions-league/ChampionsLeagueTable.tsx
@@ -4,12 +4,31 @@ import React from "react";
 
 
 const ChampionsLeagueTable: React.FC = () => {
+    const { matches, loading, error } = useFetchChampionsLeagueMatches();
+    const navigate = useNavigate();
 
+    const navigateToHome = () => {
+        return navigate("/");
+    }
     return (
         <>
         {loading && (
             <div className="flex justify-center items-center min-h-screen bg-[url('/CL-bg.jpg')] bg-cover bg-center">
                 <div className="w-16 h-16 border-4 border-white border-t-transparent rounded-full animate-spin"></div>
+            </div>
+        )}
+        { error && (
+            <div className="flex justify-center items-center min-h-screen bg-[url('/CL-bg.jpg')] bg-cover bg-center px-4">
+                <div className="bg-white/90 backdrop-blur-md p-8 rounded-2xl shadow-2xl text-center max-w-md w-full">
+                    <h2 className="text-3xl font-extrabold text-red-600 mb-4">Oops! Something went wrong</h2>
+                    <p className="text-gray-800 text-base mb-6">{error || "An unexpected error occurred. Please try again later."}</p>
+                    <button
+                    onClick={() => navigateToHome()}
+                    className="px-6 py-2 bg-gradient-to-r cursor-pointer from-gray-400 to-gray-600 text-white font-semibold rounded-lg hover:from-gray-500 hover:to-gray-700 transition duration-300"
+                    >
+                    Go back to the homepage
+                    </button>
+                </div>
             </div>
         )}
         <div className="p-6 bg-[url('/CL-bg.jpg')] bg-cover bg-center min-h-screen">
