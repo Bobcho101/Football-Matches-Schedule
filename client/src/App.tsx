36ea081e40dc1979233d8aa7da6627ff18c88d07
Bobcho101 <bgbobi878@gmail.com>
2025-04-15 22:46:57 +0300

    added tghe new table for premeir league

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import ChampionsLeagueTable from './components/champions-league/ChampionsLeagueTable'
 import { Route, Routes } from 'react-router-dom'
 import Home from './components/home/Home'
+import PremierLeagueTable from './components/premier-league/PremierLeagueTable'
 
 function App() {
     return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/league/champions-league' element={<ChampionsLeagueTable />} />
+            <Route path='/league/premier-league' element={<PremierLeagueTable />} />
         </Routes>
         </>
     )
