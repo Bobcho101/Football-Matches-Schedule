ed04f5afb999b1f6533ca144b647fccf45ac58e9
Bobcho101 <bgbobi878@gmail.com>
2025-04-16 16:53:33 +0300

    added nav component

---
+++ client/src/components/navigation/Navigation.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+const Navigation: React.FC = () => {
+    const navigate = useNavigate();
+
+    const navigateToLeague = (name: string) => {
+        return navigate(`/league/${name}`);
+    }
+
+    return (
+        <div className="flex flex-wrap justify-center gap-4 mb-8">
+            <button onClick={() => navigateToLeague('champions-league')} className="px-6 py-3 text-lg cursor-pointer bg-blue-300 text-gray-800 rounded-lg shadow-md hover:bg-blue-400 transition duration-300 focus:outline-none focus:ring-2 focus:ring-blue-200">
+                Champions League
+            </button>
+            <button onClick={() => navigateToLeague('premier-league')} className="px-6 py-3 text-lg cursor-pointer bg-yellow-300 text-gray-800 rounded-lg shadow-md hover:bg-yellow-400 transition duration-300 focus:outline-none focus:ring-2 focus:ring-yellow-200">
+                Premier League
+            </button>
+            <button onClick={() => navigateToLeague('la-liga')} className="px-6 py-3 text-lg cursor-pointer bg-red-300 text-gray-800 rounded-lg shadow-md hover:bg-red-400 transition duration-300 focus:outline-none focus:ring-2 focus:ring-red-200">
+                La Liga
+            </button>
+            <button onClick={() => navigateToLeague('ligue-1')} className="px-6 py-3 text-lg cursor-pointer bg-green-300 text-gray-800 rounded-lg shadow-md hover:bg-green-400 transition duration-300 focus:outline-none focus:ring-2 focus:ring-green-200">
+                Ligue 1
+            </button>
+        </div>
+    );
+};
+
+export default Navigation;
--- client/src/components/premier-league/PremierLeagueTable.tsx
+++ client/src/components/premier-league/PremierLeagueTable.tsx
@@ -1,4 +1,3 @@
-import { useNavigate } from "react-router-dom";
 import { useFetchMatches } from "../../api/matchesApi";
 import React from "react";
 import { setDocumentTitle } from "../../utils/document";
@@ -9,36 +8,16 @@ import LoadingSpinner from "../loading/LoadingSpinner";
 const PremierLeagueTable: React.FC = () => {
     setDocumentTitle("Premier League");
     const { matches, loading, error } = useFetchMatches("pl");
-    const navigate = useNavigate();
-
-    const navigateToLeague = (name: string) => {
-        return navigate(`/league/${name}`);
-    }
 
     return (
         <>
-        {loading && <LoadingSpinner />}
+        { loading && <LoadingSpinner />}
         { error && <Error error={error} />}
-        <div className="p-6 bg-[url('/premier-bg.jpg')] bg-center min-h-screen">
-            <div className="flex flex-wrap justify-center gap-4 mb-8">
-                <button onClick={() => navigateToLeague('champions-league')} className="px-6 py-3 text-lg cursor-pointer bg-blue-300 text-gray-800 rounded-lg shadow-md hover:bg-blue-400 transition duration-300 focus:outline-none focus:ring-2 focus:ring-blue-200">
-                    Champions League
-                </button>
-                <button onClick={() => navigateToLeague('premier-league')} className="px-6 py-3 text-lg cursor-pointer bg-yellow-300 text-gray-800 rounded-lg shadow-md hover:bg-yellow-400 transition duration-300 focus:outline-none focus:ring-2 focus:ring-yellow-200">
-                    Premier League
-                </button>
-                <button onClick={() => navigateToLeague('la-liga')} className="px-6 py-3 text-lg cursor-pointer bg-red-300 text-gray-800 rounded-lg shadow-md hover:bg-red-400 transition duration-300 focus:outline-none focus:ring-2 focus:ring-red-200">
-                    La Liga
-                </button>
-                <button onClick={() => navigateToLeague('ligue-1')} className="px-6 py-3 text-lg cursor-pointer bg-green-300 text-gray-800 rounded-lg shadow-md hover:bg-green-400 transition duration-300 focus:outline-none focus:ring-2 focus:ring-green-200">
-                    Ligue 1
-                </button>
-            </div>
+        <div className="p-6 bg-[url('/premier-bg.jpg')] bg-center min-h-screen">   
             <h2 className="text-4xl font-extrabold mb-6 text-white text-center drop-shadow-[0_2px_4px_rgba(0,0,0,0.6)]">Upcoming Matches</h2>
             <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
             {!error && matches.map((match) => { 
                 if(match.homeTeam.shortName === null || match.awayTeam.shortName === null) return;
-
                 return(
                     <div key={match.id} className="bg-white/70 backdrop-blur-md p-6 rounded-2xl shadow-2xl hover:shadow-[0_10px_25px_rgba(0,0,0,0.3)] transition duration-300 border border-white/30 ring-1 ring-black/10">
                         <div className="text-center text-xl font-semibold text-gray-700 mb-2">
