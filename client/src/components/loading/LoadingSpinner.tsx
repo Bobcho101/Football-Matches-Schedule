4aa30158e573e5f7a284b3c27f7a4fb6f92cb1c8
Bobcho101 <bgbobi878@gmail.com>
2025-04-16 16:50:21 +0300

    changed the components so i can add their type

--- client/src/components/loading/LoadingSpinner.tsx
+++ client/src/components/loading/LoadingSpinner.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 
-export default function LoadingSpinner() {
+const LoadingSpinner: React.FC = () => {
     return (
-        <div className="flex justify-center items-center min-h-screen bg-[url('/CL-bg.jpg')] bg-cover bg-center">
+        <div className="flex justify-center items-center min-h-screen bg-black bg-cover bg-center">
             <div className="w-16 h-16 border-4 border-white border-t-transparent rounded-full animate-spin"></div>
         </div>
     );
 }
+
+export default LoadingSpinner;
