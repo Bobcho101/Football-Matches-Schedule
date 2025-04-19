90f826ca131ca336b4adefc02231db0cf1e54744
Bobcho101 <bgbobi878@gmail.com>
2025-04-19 19:50:35 +0300

    added ligue 1 table and deleted the old fixed tables

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,10 +4,6 @@ import { Route, Routes } from 'react-router-dom'
 import Home from './components/home/Home'
 import NotFound from './components/not-found/NotFound'
 import LeagueTable from './components/league-table/LeagueTable'
-// import ChampionsLeagueTable from './components/champions-league/ChampionsLeagueTable'
-// import PremierLeagueTable from './components/premier-league/PremierLeagueTable'
-// import LaLigaTable from './components/la-liga/LaLiga'
-// import Ligue1Table from './components/ligue-1/Ligue-1'
 
 
 const App: React.FC = () => {
@@ -18,6 +14,7 @@ const App: React.FC = () => {
             <Route path='/league/champions-league' element={<LeagueTable name='Champions League' backgroundImg='CL-bg' backgroundCover={true} leagueId='CL' />} />
             <Route path='/league/premier-league' element={<LeagueTable name='Premier League' backgroundImg='premier-bg' backgroundCover={false} leagueId='PL' />} />
             <Route path='/league/la-liga' element={<LeagueTable name='La Liga' backgroundImg='la-liga-bg' backgroundCover={false} leagueId='PD' />} />
+            <Route path='/league/ligue-1' element={<LeagueTable name='Ligue 1' backgroundImg='ligue-1-bg' backgroundCover={false} leagueId='FL1' />} />
             <Route path='*' element={<NotFound />} />
         </Routes>
         </>
