5d348b5827b2aa02acaa7cef3601165b84fe5d30
Bobcho101 <bgbobi878@gmail.com>
2025-04-19 18:09:39 +0300

    added a new no matches avaible component

---
+++ client/src/components/no-matches-available/NoMatchesAvailable.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const NoMatchesAvailable: React.FC = () => {
+    return (
+        <div className="text-white text-xl font-semibold col-span-full text-center mt-10">
+            No upcoming matches available.
+        </div>
+    );
+};
+
+export default NoMatchesAvailable;
